993d4ea8e754f579cfd410948172f76356f4bf81
Olal Keith <jkeneth.jk@gmail.com>
2018-10-09 16:10:57 +0300

    [feature-161076129]Create a new sale attendant user account

--- UI/js/main.js
+++ UI/js/main.js
@@ -3,3 +3,12 @@ $(document).ready(function() {
 		$('.side-nav').toggleClass('visible');
 	});
 });
+
+// If user clicks anywhere outside of the modal, Modal will close
+
+var modal = document.getElementById('modal-wrapper');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
