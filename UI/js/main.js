f9210e942a81cdb93a9e671cf8ef205423d399a8
Olal Keith <jkeneth.jk@gmail.com>
2018-10-10 21:43:56 +0300

    [feature-161130305]make the admin​ assign a product to a specific category.

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-	$('.nav-trigger').click(function() {
-		$('.side-nav').toggleClass('visible');
-	});
+$('.nav-trigger').click(function() {
+$('.side-nav').toggleClass('visible');
 });
-
+});
 // If user clicks anywhere outside of the modal, Modal will close
-
 var modal = document.getElementById('modal-wrapper');
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+if (event.target == modal) {
+modal.style.display = "none";
+}
 }
